580f3dc6b033d133a222b4c631f390d0c526131b
Joel del Pilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-04-25 20:44:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/GridWars/',
+  define: {
+    'process.env': {},
+  },
+  build: {
+    target: 'esnext',
+  },
   plugins: [vue()],
   resolve: {
     alias: {
